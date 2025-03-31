9bc1bda1e09a7faed9102d50d7cf10e0384173ef
pushpam5 <pushpamsxn58@gmail.com>
2025-03-31 15:03:09 +0530

    fix: button css

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-export const RECORDING_URL = process.env.REACT_APP_RECORDING_URL || '';
-export const ROOM_CODE = process.env.REACT_APP_ROOM_CODE || '';
+export const RECORDING_URL = process.env.REACT_APP_RECORDING_URL || 'https://pub-3ffeeab1731249e489e4e1c1710bc8e5.r2.dev/file.mp3';
+export const ROOM_CODE = process.env.REACT_APP_ROOM_CODE || 'hdf-udxp-bxn';
